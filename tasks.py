1e551d959a16d9549a8e8fda76fe3cdbc7c19571
hatonlee <hatonen.leevi@gmail.com>
2025-11-25 23:46:25 +0200

    feat: added basic invoke commands

---
+++ tasks.py
@@ -0,0 +1,32 @@
+from invoke import task
+
+
+@task
+def start(ctx):
+    ctx.run("python src/app.py")
+
+
+@task
+def test(ctx):
+    ctx.run("pytest src/tests")
+
+
+@task
+def coverage(ctx):
+    ctx.run("coverage run --branch -m pytest")
+
+
+@task(coverage)
+def coverage_report(ctx):
+    ctx.run("coverage html")
+
+
+@task()
+def format(ctx):
+    ctx.run("isort .")
+    ctx.run("black .")
+
+
+@task()
+def lint(ctx):
+    ctx.run("pylint src")
