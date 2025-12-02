f46c58ee5166ce4c7781480309039f0633b0c5ff
hatonlee <hatonen.leevi@gmail.com>
2025-12-02 19:09:53 +0200

    feat: return detatched objects instead of dicts

--- src/repositories/habit_repository.py
+++ src/repositories/habit_repository.py
@@ -1,7 +1,6 @@
 """Repository layer class for habit."""
 
 from sqlalchemy import select
-from sqlalchemy.inspection import inspect
 from sqlalchemy.orm import sessionmaker
 
 from db import session as default_session
@@ -14,35 +13,33 @@ class HabitRepository:
     def __init__(self, session: sessionmaker = default_session):
         self._session = session
 
-    def _to_dict(self, obj):
-        """Returns a dictionary representation of the object"""
-        return {c.key: getattr(obj, c.key) for c in inspect(obj).mapper.column_attrs}
-
     def get_all(self):
-        """Get data of one unspecified habit"""
+        """Get all habits."""
         with self._session() as session:
             habits = session.scalars(select(Habit))
-            return [self._to_dict(habit) for habit in habits]
+            return list(habits)
 
-    def get(self):
-        """Get data of all habits"""
+    def get(self, habit_id: int = None):
+        """Get habit with a specific id. The first habit is returned if id is not specified"""
         with self._session() as session:
-            habit = session.scalar(select(Habit))
-            return self._to_dict(habit) if habit else None
+            if habit_id is None:
+                habit = session.scalar(select(Habit))
+            else:
+                habit = session.scalar(select(Habit).where(Habit.id == habit_id))
+            return habit
 
     def get_by_name(self, name: str):
-        """Get data of one habit with a certain name"""
+        """Get habit with a specific name"""
         with self._session() as session:
             habit = session.scalar(select(Habit).where(Habit.name == name))
-            return self._to_dict(habit) if habit else None
+            return habit
 
     def add(self, habit_data: dict):
         """Add a new habit to the database"""
-        with self._session() as session:
+        with self._session.begin() as session:
             habit = Habit(**habit_data)
             session.add(habit)
-            session.commit()
-            return self._to_dict(habit)
+            return habit
 
 
 default_habit_repository = HabitRepository()
