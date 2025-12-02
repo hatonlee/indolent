53f65e05e1fe96513d7b483e47840827479a8fda
hatonlee <hatonen.leevi@gmail.com>
2025-12-02 23:43:58 +0200

    feat: added tracking for completion

--- src/models/habit.py
+++ src/models/habit.py
@@ -1,18 +1,25 @@
 """Define an SQLAlchemy model for a habit."""
 
+from datetime import datetime
+
 from sqlalchemy.orm import Mapped, mapped_column
 
 from db import Base
 
 
 class Habit(Base):
-    """Define an SQLAlchemy model for a habit."""
-
     __tablename__ = "habits"
 
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(unique=True)
     description: Mapped[str]
+    start_time: Mapped[datetime]
+    frequency: Mapped[int]
+    last_done: Mapped[datetime] = mapped_column(nullable=True)
 
     def __repr__(self):
-        return f"Habit(id={self.id!r}, name={self.name!r}, description={self.description!r})"
+        return f"Habit(\
+            id={self.id}, name={self.name}, description={self.description},\
+            frequency{self.frequency}, start_time={self.start_time},\
+            frequency={self.frequency}, last_done={self.last_done}\
+        )"
