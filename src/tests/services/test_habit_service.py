678664ef018ff196a66a8c9add163f6f917785c6
hatonlee <hatonen.leevi@gmail.com>
2025-12-21 21:12:48 +0200

    test[100%]: add, get, find, complete

--- src/tests/services/test_habit_service.py
+++ src/tests/services/test_habit_service.py
@@ -1,5 +1,4 @@
-import unittest
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from repositories.habit_repository import HabitRepository
 from services.habit_service import HabitService
@@ -9,24 +8,42 @@ from tests.base import BaseTestCase
 class TestHabitService(BaseTestCase):
     def setUp(self):
         super().setUp()
-        repo = HabitRepository(session=self.session)
-        self.service = HabitService(repo=repo)
+        repo = HabitRepository(self.db)
+        self.service = HabitService(repo)
 
         self.data = {
-            "name": "name",
-            "description": "description",
+            "name": "test_name",
+            "description": "test_description",
             "start_time": datetime.now(),
-            "frequency": 60,
+            "frequency": timedelta(days=1),
         }
 
-    def test_get_returns_habit(self):
-        self.service.add(self.data)
-        habit = self.service.get()
+    def test_add(self):
+        habit = self.service.add(self.data)
         self.assertIsNotNone(habit)
-        self.assertEqual(habit.name, "name")
+        self.assertIsNotNone(habit.id)
 
-    def test_add_duplicate_fails(self):
+    def test_add_duplicate_name_raises(self):
         self.service.add(self.data)
-
         with self.assertRaises(ValueError):
             self.service.add(self.data)
+
+    def test_get(self):
+        habit_added = self.service.add(self.data)
+        habit = self.service.get(habit_added.id)
+        self.assertIsNotNone(habit)
+        if habit:
+            self.assertEqual(habit.name, "test_name")
+
+    def test_find(self):
+        self.service.add(self.data)
+        habits = self.service.find(name="test_name")
+        self.assertIsInstance(habits, list)
+        self.assertEqual(len(habits), 1)
+        self.assertEqual(habits[0].name, "test_name")
+
+    def test_complete(self):
+        habit = self.service.add(self.data)
+        completed_habit = self.service.complete(habit.id, datetime.now())
+        self.assertIsNotNone(completed_habit)
+        self.assertEqual(len(completed_habit.completions), 1)
