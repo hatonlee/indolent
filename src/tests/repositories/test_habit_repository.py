cf8a3bbd3e3916397f3d349f0e53ba8f673fecaa
hatonlee <hatonen.leevi@gmail.com>
2025-12-21 21:12:24 +0200

    test[100%]: add, get, find, complete

--- src/tests/repositories/test_habit_repository.py
+++ src/tests/repositories/test_habit_repository.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from repositories.habit_repository import HabitRepository
 from tests.base import BaseTestCase
@@ -8,13 +8,13 @@ class TestHabitRepository(BaseTestCase):
     def setUp(self):
         super().setUp()
 
-        self.repo = HabitRepository(session=self.session)
+        self.repo = HabitRepository(self.db)
 
         self.data = {
-            "name": "name",
-            "description": "description",
+            "name": "test_name",
+            "description": "test_description",
             "start_time": datetime.now(),
-            "frequency": 60,
+            "frequency": timedelta(days=1),
         }
 
     def test_add(self):
@@ -26,19 +26,62 @@ class TestHabitRepository(BaseTestCase):
         self.repo.add(self.data)
         habit = self.repo.get()
         self.assertIsNotNone(habit)
-        self.assertEqual(habit.name, "name")
+        if habit:
+            self.assertEqual(habit.name, "test_name")
 
-    def test_get_all(self):
+    def test_find(self):
         self.repo.add(self.data)
-        habits = self.repo.get_all()
+        habits = self.repo.find()
         self.assertIsInstance(habits, list)
 
-    def test_get_all_has_done_attribute(self):
+    def test_find_with_filters(self):
         self.repo.add(self.data)
-        habits = self.repo.get_all()
-        self.assertTrue(hasattr(habits[0], "done"))
+        habits = self.repo.find(name="test_name")
+        self.assertIsInstance(habits, list)
+        self.assertGreaterEqual(len(habits), 1)
 
-    def test_mark_done_sets_last_done(self):
+    def test_find_with_invalid_filter(self):
+        self.repo.add(self.data)
+        with self.assertRaises(ValueError):
+            self.repo.find(invalid_filter="value")
+
+    def test_find_with_none(self):
+        self.repo.add(self.data)
+        habits = self.repo.find(name=None)
+        self.assertIsInstance(habits, list)
+        self.assertGreaterEqual(len(habits), 1)
+
+    def test_complete(self):
         habit = self.repo.add(self.data)
-        self.repo.mark_done(habit.id)
-        self.assertIsNotNone(habit.last_done)
+        completed = self.repo.complete(habit.id, datetime.now())
+        self.assertIsNotNone(completed)
+        self.assertEqual(len(completed.completions), 1)
+
+    def test_complete_nonexistent_habit(self):
+        with self.assertRaises(ValueError):
+            self.repo.complete(-1, datetime.now())
+
+    def test_complete_different_interval(self):
+        habit = self.repo.add(self.data)
+        self.repo.complete(habit.id, datetime.now())
+        self.repo.complete(habit.id, datetime.now() + timedelta(days=1))
+        completed = self.repo.get(habit.id)
+        self.assertIsNotNone(completed)
+        if completed:
+            self.assertEqual(len(completed.completions), 2)
+
+    def test_complete_same_interval(self):
+        habit = self.repo.add(self.data)
+        self.repo.complete(habit.id, datetime.now())
+        self.repo.complete(habit.id, datetime.now())
+        completed = self.repo.get(habit.id)
+        self.assertIsNotNone(completed)
+        if completed:
+            self.assertEqual(len(completed.completions), 1)
+
+    def test_delete_all(self):
+        self.repo.add(self.data)
+        self.repo.delete_all()
+        habits = self.repo.find()
+        self.assertIsInstance(habits, list)
+        self.assertEqual(len(habits), 0)
