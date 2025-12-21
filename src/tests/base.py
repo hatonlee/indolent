9460643a4a299b4bdc75d69260cccf4cb0124012
hatonlee <hatonen.leevi@gmail.com>
2025-12-21 21:09:18 +0200

    fix(test): use DB class in setup

--- src/tests/base.py
+++ src/tests/base.py
@@ -1,15 +1,11 @@
 import unittest
 
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
-from db import Base
+from db import DB
 
 
 class BaseTestCase(unittest.TestCase):
     """Base test case that sets up an in-memory SQLite DB and repository."""
 
     def setUp(self):
-        engine = create_engine("sqlite:///:memory:", echo=False)
-        self.session = sessionmaker(engine, expire_on_commit=False)
-        Base.metadata.create_all(bind=engine)
+        self.db = DB(memory=True)
+        self.db.create_metadata()
